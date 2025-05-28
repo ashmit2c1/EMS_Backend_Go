d132574fe5bb20789ff355a1cab081f057d18d27
ashmit2c1 <codeatashmit@gmail.com>
2025-05-28 14:54:16 +0530

    Event Model set up and API testing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"ems_backend_go/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,10 +11,25 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 	server.Run(":8080")
 
 }
 
 func getEvents(cntxt *gin.Context) {
-	cntxt.JSON(http.StatusOK, gin.H{"message": "GET Request"})
+	events := models.GetAllEvents()
+	cntxt.JSON(http.StatusOK, gin.H{"message": "GET Request", "events": events})
+}
+
+func createEvent(cntxt *gin.Context) {
+	var event models.Event
+	err := cntxt.ShouldBindJSON(&event)
+
+	if err != nil {
+		cntxt.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data", "error": err})
+	}
+	event.ID = 1
+	event.CreatedBy = 1
+	event.Save()
+	cntxt.JSON(http.StatusCreated, gin.H{"message": "POST Request", "event": event})
 }
